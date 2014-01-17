26ad6830c8bb76cbfddfece852738e42b10f2f90
Claudiu <claudiu725@yahoo.com>
2014-01-17 18:38:47 +0200

    Copied greek_dictionary.txt and greek_sound_similarity.txt to the server so that they can be loaded by the controller.

--- src/main/java/com/ilearnrw/services/rest/TextClassificationController.java
+++ src/main/java/com/ilearnrw/services/rest/TextClassificationController.java
@@ -1,5 +1,8 @@
 package com.ilearnrw.services.rest;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import ilearnrw.languagetools.LanguageAnalyzerAPI;
 import ilearnrw.languagetools.english.EnglishLanguageAnalyzer;
 import ilearnrw.languagetools.greek.GreekLanguageAnalyzer;
@@ -9,8 +12,11 @@ import ilearnrw.textclassification.TextClassificationResults;
 import ilearnrw.user.profile.UserProfile;
 import ilearnrw.utils.LanguageCode;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,21 +33,29 @@ public class TextClassificationController {
 	
 	@Autowired
 	IProfileProvider profileProvider;
+	
+    private static final Logger log = LoggerFactory.getLogger(TextClassificationResults.class);
 
 	@RequestMapping(headers = {"Accept=application/json"},
 					value = "/text/classify/{userId}", 
 					method = RequestMethod.POST,
 					produces = "text/plain;charset=UTF-8")
-	public @ResponseBody TextClassificationResults classify(@PathVariable String userId,
-														   @Valid @RequestBody String analyzeText) throws ProfileProviderException
+	public @ResponseBody TextClassificationResults classify(HttpServletRequest request,
+														   @PathVariable String userId,
+														   @Valid @RequestBody String analyzeText) throws ProfileProviderException, FileNotFoundException
 	{
-
 		UserProfile profile = profileProvider.getProfile(userId);
 		LanguageAnalyzerAPI languageAnalyzer = null;
+		InputStream greekDictionary = request.getSession().getServletContext().getResourceAsStream("/data/greek_dictionary.txt");
+		InputStream greekSoundDictionary = request.getSession().getServletContext().getResourceAsStream("/data/greek_sound_similarity.txt");
+		if (greekDictionary == null)
+			throw new FileNotFoundException("/data/greek_sound_similarity.txt");
+		if (greekSoundDictionary == null)
+			throw new FileNotFoundException("/data/greek_sound_similarity.txt");
 		if( profile.getLanguage() == LanguageCode.EN)
 			languageAnalyzer = new EnglishLanguageAnalyzer();
 		else
-			languageAnalyzer = new GreekLanguageAnalyzer();
+			languageAnalyzer = new GreekLanguageAnalyzer(greekDictionary, greekSoundDictionary);
 
 		Text text = new Text(analyzeText, profile.getLanguage());
 		Classifier cls = new Classifier(profile, text, languageAnalyzer);
