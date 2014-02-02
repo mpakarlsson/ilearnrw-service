8f87bbb98113683ebf98fcc90db9e795886fcd6c
cantemir.mihu <cantemir.mihu@gmail.com>
2014-02-02 17:32:49 +0200

    added dummy implementation for getting next words

--- src/main/java/com/ilearnrw/api/selectnextword/SelectNextWordController.java
+++ src/main/java/com/ilearnrw/api/selectnextword/SelectNextWordController.java
@@ -1,19 +1,39 @@
 package com.ilearnrw.api.selectnextword;
 
+import ilearnrw.languagetools.greek.GreekDictionaryLoader;
+import ilearnrw.textclassification.Word;
+import ilearnrw.textclassification.greek.GreekWord;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SelectNextWordController {
 
-	private static Logger LOG = Logger.getLogger(SelectNextWordController.class);
+	private static Logger LOG = Logger
+			.getLogger(SelectNextWordController.class);
 
-	@RequestMapping(value = "/activity/words", method = RequestMethod.GET)
+	@RequestMapping(value = "/activity/next_words", method = RequestMethod.GET)
 	public @ResponseBody
-	void selectNextWords() {
-		return;
+	List<Word> selectNextWords(
+			@RequestParam(value = "activity", required = true) String activity,
+			@RequestParam(value = "count", required = true) int count,
+			@RequestParam(value = "percent_new_words", required = false) Integer percentNewWords,
+			@RequestParam(value = "only_tricky_words", required = false) Boolean onlyTrickyWords) {
+		GreekDictionaryLoader loader = new GreekDictionaryLoader();
+		List<GreekWord> subList = loader.getGreekWords().subList(0, count);
+		List<Word> result = new ArrayList<Word>();
+		for (Word w : subList) {
+			result.add(w);
+		}
+
+		return result;
 	}
 }
